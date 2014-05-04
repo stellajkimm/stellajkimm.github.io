71a6ae571b35e8ef394fef288f6231e84aaac887
stellajkimm <stellajkimm@gmail.com>
2014-05-04 13:46:39 -0400

    die 1 solution

--- unit2_projects/week_5/1_die/my_solution.rb
+++ unit2_projects/week_5/1_die/my_solution.rb
@@ -1,28 +1,37 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: sides
+# Output: number of sides or random roll
 # Steps:
+# define class Die
+# define method initialize taking 1 parameter "sides"
+# define instance variable named sides that is equal to sides
+# if sides is less than 1, raise an ArgumentError
+# define method sides
+# return instance variable sides
+# define roll
+# return a sample of an array of numbers from 1 to number of sides
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+  	@sides = sides
+  	raise ArgumentError.new("The number of sides entered is not valid") if @sides < 1
   end
   
   def sides
-    # code goes here
+  	@sides
   end
   
   def roll
-    # code goes here
+    (1..@sides).to_a.sample
   end
 end
 
@@ -36,6 +45,22 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+# 
+d1 = Die.new(7)
+d2 = Die.new(400)
+
+p d1.sides == 7
+p d2.sides == 400
+
+p d1.roll >= 1 && d1.roll <= 7
+p d2.roll >= 1 && d2.roll <= 400
+
+# not really sure how to do these tests:
+# d3 = Die.new(-1)
+# d4 = Die.new(0)
+
+# p d3.sides == 
+# p d4.sides == 
 
 
 
@@ -43,3 +68,8 @@ end
 
 
 # 5. Reflection 
+
+# If I did this correctly, I think this was an easy task.  I am still unsure of drivers test.  I need to
+# pair with someone about this.  Or read some more info on it.  This was good practice on creating a
+# simple class.  I'm not quite sure if I did the argument error right but I think I did.
+
