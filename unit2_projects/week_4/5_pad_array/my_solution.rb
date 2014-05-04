62b49bc4245ff6695a3040c2197e799214ff3c5e
stellajkimm <stellajkimm@gmail.com>
2014-05-04 07:25:51 -0400

    pad array and cipher challenge update

--- unit2_projects/week_4/5_pad_array/my_solution.rb
+++ unit2_projects/week_4/5_pad_array/my_solution.rb
@@ -4,22 +4,60 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? min size and optional pad value
+# What is the output? (i.e. What should the code return?) the array with the correct number of elements
 # What are the steps needed to solve the problem?
+# define class Array
+# define a method pad that takes in two parameters: min pad size, and pad value defaulting to nil
+# define a new variable new_array that is equal to the array (self)
+# determine the max of the difference between (the pad min size and array length) and zero and
+# push the val to the new_array that many times
+# define a method pad! that takes in two parameters: min pad size and pad value defaulting to nil
+# determine the max of the difference between (the pad min size and array length) and zero and
+# push the val to the array (self) that many times.
 
 
 # 2. Initial Solution
 
+# class Array
+# 	def pad(min_size, val = nil)
+# 		new_array = self
+# 		extra = [min_size - self.length, 0].max
+# 		extra.times { new_array << val }
+# 		new_array
+# 	end
 
-
+# 	def pad!(min_size, val = nil)
+# 		extra = [min_size - self.length, 0].max
+# 		extra.times { self << val }
+# 		self
+# 	end
+# end
 
 # 3. Refactored Solution
 
+class Array
+	def pad(min_size, val = nil)
+		new_array = self
+		[min_size - self.length, 0].max.times { new_array << val }
+		new_array
+	end
+
+	def pad!(min_size, val = nil)
+		[min_size - self.length, 0].max.times { self << val }
+		self
+	end
+end
 
 
 # 4. Reflection 
+
+# This solution is still in the works.  It is failing two tests (Array#pad operates non-destructively)
+# and (Array#pad pads elements to the end of an array).  I really don't understand why this doesnt work.
+# The pad is basically the same method as pad! except it works off a new variable called new_array
+# instead of itself and returns the new_array.  This shouldn't change the array it self at all.  It
+# also has the same exact code for producing the solution.  I need to check up on this.
--- unit2_projects/week_4/6_cipher_challenge/my_solution.rb
+++ unit2_projects/week_4/6_cipher_challenge/my_solution.rb
@@ -80,11 +80,11 @@ end
 # Driver Code:
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
 # Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
 
 # Reflection
  
