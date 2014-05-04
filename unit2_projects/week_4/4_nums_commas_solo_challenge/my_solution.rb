a1118f4af327d7c8747e9fa2bd3cbcaab72f6725
stellajkimm <stellajkimm@gmail.com>
2014-05-03 20:01:26 -0400

    solo challenge

--- unit2_projects/week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ unit2_projects/week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -7,14 +7,32 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an integer without commas
+# What is the output? (i.e. What should the code return?) the integer with commas
 # What are the steps needed to solve the problem?
+# define a method called separate_comma with a parameter num
+# turn the num to a string
+# determine the amount of digits in the integer (amount of characters in the string)
+# if amount of digits is 3 or less, return num
+# else if the number of digits is divisible evenly by 3, scan num_s and join it with a ","
+# else return the first remainder digits (form using module) of num_s, plus a ","
+# plus the rest of the num_s that is scanned and joined by ","
 
 
 # 2. Initial Solution
 
-
+def separate_comma(num)
+	num_s = num.to_s
+	digits = num_s.length
+	if digits <= 3
+		num_s
+	elsif digits % 3 == 0
+		num_s.scan(/.../).join(",")
+	else
+		i = digits % 3
+		num_s[0..i-1] + "," + num_s[i..-1].scan(/.../).join(",")
+	end
+end
 
 
 # 3. Refactored Solution
@@ -22,3 +40,12 @@
 
 
 # 4. Reflection 
+
+# I am still having problems writing out the psuedocode before I write my solution.  I always
+# have trouble thinking of how I am going to do it until I code it.  The reason why is,
+# I am not sure how to exactly to get the solution if I don't know all of the methods I can use
+# to create a solution.  I have to work my way through trial and error.  I know psuedocode is really important
+# because if I cant think of the solution in english, how will I be able to code it?
+# But I guess this will come with practice by training my mind to think of ideas of how to
+# solve it without having to code it first.  I learned the new method "scan" by using ruby-doc.org
+# in the string section.  I already knew join, so after thinking of that, I figured it out.
