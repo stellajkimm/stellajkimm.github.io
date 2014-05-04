752bb9ff933fa86d8a801ef22882cd0c500b66f1
stellajkimm <stellajkimm@gmail.com>
2014-05-04 15:10:15 -0400

    solo challenge

--- unit2_projects/week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ unit2_projects/week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,12 +1,12 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: answer
+# Output: 
 # Steps:
 
 
@@ -14,10 +14,26 @@
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
+    @solved = false
   end
-  
-  # Make sure you define the other required methods, too
+
+  def guess(guess)
+  	if guess > @answer
+  		@solved = false
+  		:high
+  	elsif guess < @answer
+  		@solved = false
+  		:low
+  	elsif guess == @answer
+  		@solved = true
+  		:correct
+  	end
+  end
+
+  def solved?
+  	@solved
+  end
 end
 
 
@@ -31,10 +47,26 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+g = GuessingGame.new(8)
 
+p "solved? returns false before a guess is made"
+p g.solved? == false
 
+p "guess returns :high when the guess is too high"
+p g.guess(10) == :high
 
+p "guess returns :low when the guess is too low"
+p g.guess(2) == :low
+
+p "guess returns :correct when the guess is correct"
+p g.guess(8) == :correct
 
 
 
 # 5. Reflection 
+# 
+# Okay now I think I really understand the driver tests now.  I didn't realize that I had to take the tests from
+# the specs.  Anyway I think this challenge was fun.  I think the only thing that might be repetitive about my code
+# is that I put @solved = false many times and I don't know if I actually have to do that.  I learned more about
+# writing classes but I don't have the concept of symbols down yet.  I think I need to understand instance variables a little
+# more too now.
